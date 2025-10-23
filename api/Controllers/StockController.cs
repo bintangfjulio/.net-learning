b62674eacede5b2b81a516fbeec9a98ca4280954
Bintang Fajar Julio <bintangin14@gmail.com>
2025-10-23 14:14:48 +0700

    add dtos

--- api/Controllers/StockController.cs
+++ api/Controllers/StockController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using api.Data;
 using Microsoft.AspNetCore.Mvc;
+using api.Mappers;
 
 namespace api.Controllers
 {
@@ -20,7 +21,9 @@ namespace api.Controllers
         [HttpGet]
         public IActionResult GetAll()
         {
-            var stocks = _context.Stocks.ToList();
+            var stocks = _context.Stocks.ToList()
+                .Select(x => x.ToStockDto());
+
             return Ok(stocks);
         }
 
@@ -34,7 +37,7 @@ namespace api.Controllers
                 return NotFound();
             }
 
-            return Ok(stock);
+            return Ok(stock.ToStockDto());
         }
     }
 }
--- api/obj/Debug/net9.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net9.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a03df530908f184fbede2eeca895ba312373303e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2428094d76a325723d2bd265f2d50a5804fbd2a2")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

ad0e042811c37be2f7a6dfa0638ba2c3994d923c
Bintang Fajar Julio <bintangin14@gmail.com>
2025-10-24 09:41:37 +0700

    sort by

--- api/Repository/StockRepository.cs
+++ api/Repository/StockRepository.cs
@@ -47,10 +47,23 @@ namespace api.Repository
             {
                 stocks = stocks.Where(x => x.CompanyName.Contains(query.CompanyName));
             }
-            
+
             if (!string.IsNullOrWhiteSpace(query.Symbol))
             {
                 stocks = stocks.Where(x => x.Symbol.Contains(query.Symbol));
+            }
+            
+            if (!string.IsNullOrWhiteSpace(query.SortBy))
+            {
+                if (query.SortBy.Equals("Symbol", StringComparison.OrdinalIgnoreCase))
+                {
+                    stocks = query.IsDescending ? stocks.OrderByDescending(x => x.Symbol) : stocks.OrderBy(x => x.Symbol);
+                }
+                
+                if (query.SortBy.Equals("CompanyName", StringComparison.OrdinalIgnoreCase))
+                {
+                    stocks = query.IsDescending ? stocks.OrderByDescending(x => x.CompanyName) : stocks.OrderBy(x => x.CompanyName);
+                }
             }
 
             return await stocks.ToListAsync();
